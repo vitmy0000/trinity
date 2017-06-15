589ec9335b5777be4072abe7780db4b8838b4083
weiz <weiz.vit@gmail.com>
2017-06-15 12:07:47 -0400

    minor

--- test.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-with open('vimrc') as f:
-    line_num = 0
-    for line in f:
-        if line_num == int(sys.argv[1]):
-            sys.stdout.write(line.strip())
-        line_num += 1
